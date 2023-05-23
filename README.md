a29d15ab882eed0d0f3f4e287bd11f2ea3581c0f
zhengzhang <zhengzhang@ruijie.com.cn>
2023-05-23 11:16:49 +0800

    sdl

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CMAKE
 CMAKE
+¹ÙÍø
+http://www.ffmpeg.org/
+http://www.libsdl.org/
---
+++ sdl_test/main.cpp
@@ -0,0 +1,14 @@
+﻿// main.cpp: 定义应用程序的入口点。
+//
+
+#include "main.h"
+
+
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Test SDL" << std::endl;
+    getchar();
+}
