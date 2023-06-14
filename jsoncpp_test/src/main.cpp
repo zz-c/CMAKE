f12d7203274a3a982b20ad7043befe14dd7ae101
zhengzhang <zhengzhang@ruijie.com.cn>
2023-06-14 13:42:46 +0800

    json

--- jsoncpp_test/src/main.cpp
+++ jsoncpp_test/src/main.cpp
@@ -3,6 +3,7 @@
 #include <json/json.h>
 #include <json/value.h>
 #include <json/reader.h>
+#include <json/writer.h>
 
 
 int main() {
@@ -13,7 +14,11 @@ int main() {
     root["id"] = 1001;
     root["data"] = "hello world";
     std::string request = root.toStyledString();
-    std::cout << "request is " << request << std::endl;
+    std::cout << "request is:" << request << std::endl;
+    Json::FastWriter fastWriter;
+    std::string requestSimple = fastWriter.write(root);
+    std::cout << "requestSimple is:" << requestSimple << std::endl;
+
     Json::Value root2;
     Json::Reader reader;
     reader.parse(request, root2);
