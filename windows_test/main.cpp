10ec2967e8a38f75804388e121ea37a76ee296e4
zhengzhang <zhengzhang@ruijie.com.cn>
2023-07-01 16:45:34 +0800

    windows

---
+++ windows_test/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <windows.h>
+//https://blog.csdn.net/weixin_50964512/article/details/124140735
+// 窗口过程的回调函数
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // 注册窗口类
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "MyWindowClass";
+    RegisterClass(&wc);
+
+    // 创建窗口实例
+    HWND hwnd = CreateWindowEx(
+        0,
+        "MyWindowClass",
+        "Test Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    // 显示窗口
+    ShowWindow(hwnd, nCmdShow);
+
+    // 消息循环
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
