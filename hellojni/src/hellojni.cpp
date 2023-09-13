b527dd67e74dde5acdf33d14ef8aec0c2ea7ae17
zhengzhang <zhengzhang@ruijie.com.cn>
2023-09-13 17:13:57 +0800

    hellojni

---
+++ hellojni/src/hellojni.cpp
@@ -0,0 +1,11 @@
+// hellojni.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+
+#include "top_zhost_jni_HelloWorldJNI.h"
+
+
+JNIEXPORT void JNICALL Java_top_zhost_jni_HelloWorldJNI_helloJNI
+(JNIEnv *, jobject, jstring){
+	printf("C printf from cmake3.8 ..");
+}
