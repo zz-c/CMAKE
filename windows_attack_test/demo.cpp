d5b6699877e34125ef56dd575c7e594bf75f132f
zhengzhang <zhengzhang@ruijie.com.cn>
2024-08-19 10:39:01 +0800

    windowsapi

--- windows_attack_test/demo.cpp
+++ windows_attack_test/demo.cpp
@@ -53,8 +53,8 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     char windowTitle[256];
     // 获取窗口标题
     if (GetWindowText(hwnd, windowTitle, sizeof(windowTitle))) {
-        // 检查标题是否包含“记事本”
-        if (strstr(windowTitle, "Demo Window") != NULL) {
+        // 检查标题是否包含“记事本”网络调试助手 Demo Window
+        if (strstr(windowTitle, "网络调试助手") != NULL) {
             printf("句柄: %p, 标题: %s\n", hwnd, windowTitle);
             // 找到目标窗口，保存句柄并停止枚举
             HWND* pHwnd = (HWND*)lParam;
@@ -75,7 +75,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             50, 50, 200, 25, hwnd, (HMENU)IDC_EDIT, hInst, NULL);
         CreateWindowEx(0, "BUTTON", "获取句柄", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
             50, 100, 80, 30, hwnd, (HMENU)ID_SAVE_BUTTON, hInst, NULL);
-        CreateWindowEx(0, "BUTTON", "修改内存", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        CreateWindowEx(0, "BUTTON", "读取内存", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
             150, 100, 80, 30, hwnd, (HMENU)ID_SHOW_BUTTON, hInst, NULL);
         CreateWindowEx(0, "BUTTON", "事件触发", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
             250, 100, 80, 30, hwnd, (HMENU)ID_SHOW_BUTTON, hInst, NULL);
@@ -100,9 +100,59 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
         case ID_SHOW_BUTTON:
         {
-            char msg[200];
-            snprintf(msg, sizeof(msg), "Saved value: %d", value);
-            MessageBox(hwnd, msg, "提示", MB_OK);
+            // 获取窗口句柄
+            HWND hwnd = NULL;
+            EnumWindows(EnumWindowsProc, (LPARAM)&hwnd);
+            if (hwnd == NULL) {
+                MessageBox(hwnd, "无法找到窗口句柄", "提示", MB_OK);
+                return 0;
+            }
+            // 获取进程di
+            DWORD processId;
+            DWORD threadId = GetWindowThreadProcessId(hwnd, &processId);
+            if (threadId != 0) {
+                printf("Thread ID: %lu\n", threadId);
+                printf("Process ID: %lu\n", processId);
+            }
+            else {
+                printf("Failed to get thread and process IDs. Error: %lu\n", GetLastError());
+            }
+            // 读取内存
+
+            HANDLE hProcess;
+            LPCVOID baseAddress = (LPCVOID)0x024831BC; // 目标进程中的起始地址
+            int buffer;
+            SIZE_T bytesRead;
+
+            // 打开目标进程
+            // hProcess = OpenProcess(PROCESS_VM_READ, FALSE, processId);
+            hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, processId);
+            if (hProcess == NULL) {
+                printf("Failed to open process. Error: %lu\n", GetLastError());
+                return 1;
+            }
+            else {
+                printf("hProcess ID: %lu\n", hProcess);
+            }
+
+
+            // 读取数据
+            if (ReadProcessMemory(hProcess, baseAddress, &buffer, sizeof(buffer), &bytesRead)) {
+                printf("Successfully read %zu bytes. Value: %d\n", bytesRead, buffer);
+            }
+            else {
+                // printf("Failed to read memory. Error: %lu\n", GetLastError());
+                DWORD error = GetLastError();
+                if (error == ERROR_PARTIAL_COPY) {
+                    printf("Failed to read memory. Error: %lu (Partial copy)\n", error);
+                }
+                else {
+                    printf("Failed to read memory. Error: %lu\n", error);
+                }
+            }
+
+            // 关闭进程句柄
+            CloseHandle(hProcess);
         }
         break;
         }
