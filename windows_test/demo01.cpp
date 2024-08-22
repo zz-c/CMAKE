908563c5b084d7c64a0defa802a623052425bccb
zhengzhang <zhengzhang@ruijie.com.cn>
2024-08-22 14:45:20 +0800

    wintest

--- windows_test/demo01.cpp
+++ windows_test/demo01.cpp
@@ -1,13 +1,12 @@
 ﻿#include <windows.h>
 #include <stdio.h>
 
-#define ID_SAVE_BUTTON 1
-#define ID_SHOW_BUTTON 2
-#define IDC_EDIT 3
+#define ID_EDIT 1
+#define ID_SAVE_BUTTON 2
+#define ID_SHOW_BUTTON 3
 #define ID_TEXT_AREA 4
 int value = 0;; // 用于保存输入的数字变量
-HWND hEdit;
-
+HWND hEdit, hTextArea;
 
 // 窗口过程的回调函数
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -28,16 +27,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         "Demo Window",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        800, 600,
+        400, 300,
         NULL,
         NULL,
         hInstance,
         NULL
     );
-
     // 显示窗口
     ShowWindow(hwnd, nCmdShow);
-
     // 消息循环
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0))
@@ -49,60 +46,53 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return 0;
 }
 
-void save(HWND hwnd) {
+void save() {
     // 获取编辑框中的文本
     char buffer[100];
     GetWindowText(hEdit, buffer, sizeof(buffer));
     // 将文本转换为整数
     value = strtol(buffer, NULL, 10);
-    MessageBox(hwnd, "保存成功", "提示", MB_OK);
+    SetWindowText(hTextArea, "保存成功");
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    
+void read() {
+    // 显示保存的文本
+    char msg[200];
+    snprintf(msg, sizeof(msg), "当前值: %d", value);
+    SetWindowText(hTextArea, msg);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
     switch (uMsg)
     {
-    case WM_CREATE:
-        // 创建编辑框
-        hEdit = CreateWindowEx(0, "EDIT", "0", WS_CHILD | WS_VISIBLE | WS_BORDER,
-            50, 50, 200, 25, hwnd, (HMENU)IDC_EDIT, NULL, NULL);
-        // 创建保存按钮
-        CreateWindowEx(0, "BUTTON", "保存", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            50, 100, 80, 30, hwnd, (HMENU)ID_SAVE_BUTTON, NULL, NULL);
-        // 创建显示按钮
-        CreateWindowEx(0, "BUTTON", "读取", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            150, 100, 80, 30, hwnd, (HMENU)ID_SHOW_BUTTON, NULL, NULL);
-        // 信息提示
-        CreateWindowEx(0, "STATIC", "", WS_CHILD | WS_VISIBLE,
-            50, 150, 200, 30, hwnd, (HMENU)ID_TEXT_AREA, NULL, NULL);
-        return 0;
-    case WM_COMMAND:
-        switch (LOWORD(wParam)) {
-            case ID_SAVE_BUTTON:
-            {
-                //char buffer[100];
-                //GetWindowText(hEdit, buffer, sizeof(buffer));
-                //value = strtol(buffer, NULL, 10);
-                //MessageBox(hwnd, "保存成功", "提示", MB_OK);
-                save(hwnd);
+        case WM_CREATE:
+            // 创建编辑框
+            hEdit = CreateWindowEx(0, "EDIT", "0", WS_CHILD | WS_VISIBLE | WS_BORDER,
+                50, 50, 200, 25, hwnd, (HMENU)ID_EDIT, NULL, NULL);
+            // 创建保存按钮
+            CreateWindowEx(0, "BUTTON", "保存", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                50, 100, 80, 30, hwnd, (HMENU)ID_SAVE_BUTTON, NULL, NULL);
+            // 创建显示按钮
+            CreateWindowEx(0, "BUTTON", "读取", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                150, 100, 80, 30, hwnd, (HMENU)ID_SHOW_BUTTON, NULL, NULL);
+            // 信息提示
+            hTextArea = CreateWindowEx(0, "STATIC", "", WS_CHILD | WS_VISIBLE,
+                50, 150, 200, 20, hwnd, (HMENU)ID_TEXT_AREA, NULL, NULL);
+            return 0;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case ID_SAVE_BUTTON:
+                    save();
+                    break;
+                case ID_SHOW_BUTTON:
+                    read();
+                    break;
             }
-            break;
-
-            case ID_SHOW_BUTTON:
-            {
-                // 显示保存的文本
-                char msg[200];
-                snprintf(msg, sizeof(msg), "Saved value: %d", value);
-                MessageBox(hwnd, msg, "提示", MB_OK);
-            }
-            break;
-        }
-        return 0;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
