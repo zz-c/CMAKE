75acc06c75c78ad58babb5309ad9099ad3de70e7
zhengzhang <zhengzhang@ruijie.com.cn>
2024-11-29 11:44:07 +0800

    boost

---
+++ boost_test/main.cpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+int main() {
+	printf("boost");
+	boost::filesystem::path p{"."};
+	std::cout << "Current path: " << p << std::endl;
+	getchar();
+
+
+	return 0;
+}
